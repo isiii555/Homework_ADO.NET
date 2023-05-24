567574c7b8d5540d656bff160b1bd63f7a43ede7
İslam Salamzadə <islam.salamzade.191@gmail.com>
2023-05-24 17:35:58 +0400

    FirstTask Ado.Net

---
+++ AdoFirst/AdoFirst/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using AdoFirst.View;
+using AdoFirst.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdoFirst
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var view = new MainView();
+            view.Show();
+            var viewModel = new MainViewModel();
+            SqlDataReader reader = null;
+            try
+            {
+                viewModel.Connection.Open();
+                SqlCommand command = new SqlCommand("Select Id From Authors", viewModel.Connection);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    viewModel.AuthorsId.Add(Convert.ToInt32(reader[0]));
+                }
+                command = new SqlCommand("Select Id From Categories", viewModel.Connection);
+                reader.Close();
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    viewModel.CategoriesId.Add(Convert.ToInt32(reader[0]));
+                }
+            }
+            finally
+            {
+                reader.Close();
+                viewModel.Connection.Close();
+            }
+            view.DataContext = viewModel;
+        }
+    }
+}
---
+++ AdoFirst/AdoFirst/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdoFirst.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private int category;
+
+        private DataTable data = new DataTable();
+
+        private DataView view = new DataView();
+
+        private int author;
+        public int Author { get => author; set => Set(ref author, value); }
+        public int Category { get => category; set => Set(ref category, value); }
+
+        public ObservableCollection<int> AuthorsId { get; set; } = new();
+        public ObservableCollection<int> CategoriesId { get; set; } = new();
+
+        public DataTable Data { get => data; set => Set(ref data, value); }
+
+        public DataView View { get => view; set => Set(ref view,value); }
+
+        public SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+
+        public RelayCommand AuthorChangedCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                try
+                {
+                    Data.Clear();
+                    SqlDataAdapter sqlDataAdapter = new("Select * From Books Where Id_Category = @C_id And Id_Author = @A_id", Connection);
+                    sqlDataAdapter.SelectCommand.Parameters.Add("@C_id", SqlDbType.Int).Value = Category;
+                    sqlDataAdapter.SelectCommand.Parameters.Add("@A_id", SqlDbType.Int).Value = Author;
+                    sqlDataAdapter.Fill(dataTable: Data);
+                    View = Data.DefaultView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Data not found");
+                }
+            });
+        }
+    }
+}
